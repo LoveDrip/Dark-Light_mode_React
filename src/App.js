a87220dafd536396998c62e8097a93075b19f481
James Richbond <richbondjames@gmail.com>
2021-12-14 16:04:29 +0900

    add Toogle

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { useDarkMode } from './useDarkMode';
 import { lightTheme, darkTheme } from './theme';
 import { GlobalStyles } from './global';
+import Toggle from './Toggle';
 
 function App() {
+  const [theme, toggleTheme] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={themeMode}>
       <>
         <GlobalStyles />
-        <button>Toggle theme</button>
-        <h1>It's a light theme!</h1>
-        <footer>
-        </footer>
+        <Toggle theme={theme} toggleTheme={toggleTheme} />
+        <h1>It's a {theme === 'light' ? 'light theme' : 'dark theme'}!</h1>
       </>
     </ThemeProvider>
   );
 }
-
 
 export default App;
---
+++ src/Toggle.js
@@ -0,0 +1,22 @@
+// Toggle.js
+import React from 'react'
+import { func, string } from 'prop-types';
+import styled from 'styled-components';
+// Import a couple of SVG files we'll use in the design: https://www.flaticon.com
+
+const Toggle = ({ theme, toggleTheme }) => {
+  const isLight = theme === 'light';
+  return (
+    <button onClick={toggleTheme} >
+      <img src= "./icons/sun.svg" />
+      <img src= "./icons/moon.svg" />
+    </button>
+  );
+};
+
+Toggle.propTypes = {
+  theme: string.isRequired,
+  toggleTheme: func.isRequired,
+}
+
+export default Toggle;
